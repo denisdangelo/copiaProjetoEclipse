093eeb71dfb762968a64645a512b326683e96532
DenisDS <denis.dsilveira1@outloog.com>
2025-05-12 22:31:22 -0300

    primeiro backup do projeto

---
+++ produto/src/main/java/controller/Controller.java
@@ -0,0 +1,44 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.DAO; //importação da classe do banco de dados
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet(urlPatterns = {"/main"})
+
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	//criando o objeto dentro da classe do controller para importar e usar como metodo no doGET
+	DAO dao = new DAO();//testar o banco
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*testeConexao();//fazer teste com o banco*/
+		String action = request.getServletPath();
+		System.out.println(action);
+		if(action.equals("/main")) {
+			produtos(request,response);
+			}
+		}
+	protected void produtos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("cadastro.jsp");
+	}
+
+}
